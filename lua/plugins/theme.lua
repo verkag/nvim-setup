5dbc2c60978f21c33d9e2bcc9a3664ed33d02722
verkag <verkag1v@gmail.com>
2024-10-31 03:25:03 +0300

    borderless telescope

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -3,6 +3,38 @@ return {
         "folke/tokyonight.nvim",
         config = function()
             require("tokyonight").setup({
+
+                on_highlights = function(hl, c)
+                    local prompt = "#2d3149"
+                    hl.TelescopeNormal = {
+                        bg = c.bg_dark,
+                        fg = c.fg_dark,
+                    }
+                    hl.TelescopeBorder = {
+                        bg = c.bg_dark,
+                        fg = c.bg_dark,
+                    }
+                    hl.TelescopePromptNormal = {
+                        bg = prompt,
+                    }
+                    hl.TelescopePromptBorder = {
+                        bg = prompt,
+                        fg = prompt,
+                    }
+                    hl.TelescopePromptTitle = {
+                        bg = prompt,
+                        fg = prompt,
+                    }
+                    hl.TelescopePreviewTitle = {
+                        bg = c.bg_dark,
+                        fg = c.bg_dark,
+                    }
+                    hl.TelescopeResultsTitle = {
+                        bg = c.bg_dark,
+                        fg = c.bg_dark,
+                    }
+                end,
+
                 -- your configuration comes here
                 -- or leave it empty to use the default settings
                 style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
